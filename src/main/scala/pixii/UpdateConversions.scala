19f4bd9d4678959bdb6d37214a908a12da66aa9d
Alex Boisvert <alex.boisvert@gmail.com>
2013-01-16 10:27:56 -0800

    fix typo

--- src/main/scala/pixii/UpdateConversions.scala
+++ src/main/scala/pixii/UpdateConversions.scala
@@ -6,28 +6,28 @@ import scala.collection._
 /** Helpers for Table.update() */
 object UpdateConversions {
   import AttributeAction.{ADD, DELETE, PUT}
-  
+
   /** Convert a set of attribute values into `add` actions */
   def add(attributes: Map[String, AttributeValue]): Map[String, AttributeValueUpdate] = convert(attributes, ADD)
 
-  /** `add` alias for diambiguation */
+  /** `add` alias for disambiguation */
   def addAttributes(attributes: Map[String, AttributeValue]) = add(attributes)
-  
+
   /** Convert a set of attribute values into `delete` actions */
   def delete(attributes: Map[String, AttributeValue]): Map[String, AttributeValueUpdate] = convert(attributes, DELETE)
 
-  /** `delete` alias for diambiguation with Table.delete */
+  /** `delete` alias for disambiguation with Table.delete */
   def deleteAttributes(attributes: Map[String, AttributeValue]) = delete(attributes)
-  
+
   /** Convert a set of attribute values into `put` actions */
   def put(attributes: Map[String, AttributeValue]): Map[String, AttributeValueUpdate] = convert(attributes, PUT)
 
-  /** `put` alias for diambiguation with Table.put */
+  /** `put` alias for disambiguation with Table.put */
   def putAttributes(attributes: Map[String, AttributeValue]) = put(attributes)
-  
+
   /** Convert a set of attribute values into `add` actions */
   private def convert(attributes: Map[String, AttributeValue], action: AttributeAction): Map[String, AttributeValueUpdate] = {
-    attributes mapValues { value => 
+    attributes mapValues { value =>
       (new AttributeValueUpdate()
         .withAction(action)
         .withValue(value))
